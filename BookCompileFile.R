f610100cef48140d3d4d3b9a53afae352811810b
Tyson Barrett <t.barrett@aggiemail.usu.edu>
2017-01-02 21:15:00 -0700

    Through chapter 5

---
+++ BookCompileFile.R
@@ -0,0 +1,14 @@
+## Compile book
+setwd("~/Box Sync/GitHub/R_Book")
+library(bookdown)
+render_book(input = c("index.Rmd",
+                      "01-TheLanguage.Rmd",
+                      "02-Tidyverse.Rmd",
+                      "03-UnderstandData.Rmd",
+                      "04-BasicAnalyses.Rmd",
+                      "05-GLMs.Rmd",
+                      "06-MultilevelModels.Rmd",
+                      "07-OtherModels.Rmd",
+                      "08-AdvancedDataManipulation.Rmd",
+                      "09-AdvancedPlotting.Rmd",
+                      "10-LearnMore.Rmd"))
